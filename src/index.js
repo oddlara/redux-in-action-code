b1b6e4de733f264c9a158b83e918f77f42c03a45
davidlara <oscar.lr97@gmail.com>
2020-04-16 16:53:11 -0700

    webpack implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import tasks from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(tasks);
+const store = createStore(tasks, devToolsEnhancer());
+
+if(module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <Provider store={store}><NextApp /></Provider>,
+      document.getElementById('root')
+    );
+  });
+
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 ReactDOM.render(
   <Provider store={store}>
